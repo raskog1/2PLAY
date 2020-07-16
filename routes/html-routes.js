23ae077796efe01f20628c86dbeddd72f323013d
Ryan Skog <ryan.skog@outlook.com>
2020-07-15 18:07:19 -0700

    Rerouted pages, integrated passport for signup

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,28 +6,45 @@
 // =============================================================
 var path = require("path");
 
+// Requiring our custom middleware for checking if a user is logged in
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+
 // Routes
 // =============================================================
 module.exports = function(app) {
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
   // landing route loads landing.html
-  app.get("/landing", function(req, res) {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/landing.html"));
   });
 
-  // cms route loads cms.html
+  app.get("/rooms", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/roomLogin", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/roomLogin.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/mainMenu.html"));
+  });
+
+  // Dummy route to search page for testing
   app.get("/search", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/search.html"));
   });
 
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
-
-  // authors route loads author-manager.html
-  app.get("/authors", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
-  });
+  // app.get("/*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../public/landing.html"));
+  // });
 };
--- server.js
+++ server.js
@@ -4,15 +4,17 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
+const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+const db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -21,54 +23,23 @@ app.use(express.json());
 // Static directory
 app.use(express.static("public"));
 
+// We need to use sessions to keep track of our user's Login status
+app.use(session({ secret: "kcat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
 require("./routes/song-api-routes.js")(app);
 require("./routes/playlist-api-routes.js")(app);
-require("./routes/spotify-auth.js")(app);
+require("./routes/spotify-routes.js")(app);
+require("./routes/passport-api-routes")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-// // Requiring necessary npm packages
-// const express = require("express");
-// const session = require("express-session");
-// // Requiring passport as we've configured it
-// const passport = require("./config/passport");
-
-// // Setting up port and requiring models for syncing
-// const PORT = process.env.PORT || 8080;
-// const db = require("./models");
-
-// // Creating express app and configuring middleware needed for authentication
-// const app = express();
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static("public"));
-// // We need to use sessions to keep track of our user's login status
-// app.use(
-//   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Requiring our routes
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-
-// // Syncing our database and logging a message to the user upon success
-// db.sequelize.sync().then(() => {
-//   app.listen(PORT, () => {
-//     console.log(
-//       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-//       PORT,
-//       PORT
-//     );
-//   });
-// });
