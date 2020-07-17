d3168c5e2b0c734a5832c3c5a3fe554ff739316b
avnoto <alexandravnoto@gmail.com>
2020-07-16 19:52:36 -0700

    buttons push up into header

--- public/js/main-menu.js
+++ public/js/main-menu.js
@@ -1,17 +1,15 @@
 $(document).ready(function () {
-  //Same functions if user clicks on either text or media buttons. Might have to
-  //remove marginTop animation so that the buttons move into the navbar
   initClick();
 
   function initClick() {
     $('.mediaText').on('click', function () {
       $('h2').fadeOut(700, function () {});
 
-      $('.mediaButton').animate(
+      $('.mediaButton').delay(800).animate(
         {
           width: '50px',
           height: '50px',
-          marginTop: '-=300px',
+          marginTop: '-=530px',
         },
         1500
       );
@@ -19,11 +17,11 @@ $(document).ready(function () {
     $('.mediaButton').on('click', function () {
       $('h2').fadeOut(700, function () {});
 
-      var animation = $('.mediaButton').animate(
+      var animation = $('.mediaButton').delay(800).animate(
         {
           width: '50px',
           height: '50px',
-          marginTop: '-=300px',
+          marginTop: '-=530px',
         },
         1500
       );
@@ -35,5 +33,3 @@ $(document).ready(function () {
   $('#mediaButton2').on('click', function () {});
   $('#mediaButton3').on('click', function () {});
 });
-
-//hi
