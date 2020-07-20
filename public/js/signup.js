0464cfbec7bc37fe7441544ee393f085eb09c437
avnoto <alexandravnoto@gmail.com>
2020-07-20 01:59:53 -0700

    changed email input and updated db

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,44 +1,44 @@
 $(document).ready(() => {
   // Getting references to our form and input
-  const signUpForm = $("form.signup");
-  const emailInput = $("input#email-input");
-  const passwordInput = $("input#password-input");
+  const signUpForm = $('form.signup');
+  const textInput = $('input#text-input');
+  const passwordInput = $('input#password-input');
 
   // <<<<<<< HEAD
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", (event) => {
+  signUpForm.on('submit', (event) => {
     event.preventDefault();
     const userData = {
-      email: emailInput.val().trim(),
+      text: textInput.val().trim(),
       password: passwordInput.val().trim(),
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.text || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
+    signUpUser(userData.text, userData.password);
+    textInput.val('');
+    passwordInput.val('');
   });
 
   // <<<<<<< HEAD
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
-    $.post("/api/signup", {
-      email: email,
+  function signUpUser(text, password) {
+    $.post('/api/signup', {
+      text: text,
       password: password,
     })
       .then(() => {
-        window.location.replace("/members");
+        window.location.replace('/main');
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
   }
 
   function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
+    $('#alert .msg').text(err.responseJSON);
+    $('#alert').fadeIn(500);
   }
 });
