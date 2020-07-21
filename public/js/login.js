239d56f15698439245d59018ed48b97c53defce2
IPv5 <donationbuycraft@gmail.com>
2020-07-20 17:25:02 -0700

    Changed styling on existing handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,14 @@
 $(document).ready(() => {
     // Getting references to our form and inputs
     const loginForm = $("form.login");
-    const emailInput = $("input#email-input");
+    const textInput = $("input#text-input");
     const passwordInput = $("input#password-input");
 
     // When the form is submitted, we validate there's an email and password entered
     loginForm.on("submit", (event) => {
         event.preventDefault();
         const userData = {
-            text: emailInput.val().trim(),
+            text: textInput.val().trim(),
             password: passwordInput.val().trim(),
         };
 
@@ -16,9 +16,9 @@ $(document).ready(() => {
             return;
         }
 
-        // If we have an email and password we run the loginUser function and clear the form
+        // If we have a room name and password we run the loginUser function and clear the form
         loginUser(userData.text, userData.password);
-        emailInput.val("");
+        textInput.val("");
         passwordInput.val("");
     });
 
